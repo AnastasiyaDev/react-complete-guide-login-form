9ad14442e6af08d2ff40ca4106a2cfec6a80d2b6
Asya Bakhnar <abakhnar@oggettoweb.com>
2022-01-27 19:51:32 +0300

    Add auth-context, remove Login props (<AuthContext.Provider>, <AuthContext.Consumer>)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+import AuthContext from './store/auth-context';
+
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        setIsLoggedIn(true);
+    };
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+    return (
+        <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
+            <MainHeader onLogout={logoutHandler}/>
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler}/>}
+                {isLoggedIn && <Home onLogout={logoutHandler}/>}
+            </main>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
